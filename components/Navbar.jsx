c2fcf0cf881eca4245ae2b9d346133ca8e437fbf
Nycollas Pontes <nycollaspontes@gmail.com>
2022-07-29 02:03:39 -0300

    First Commit Project

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+    return (
+        <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-index-10">
+            {/* Left side */}
+            <div className="flex grow items-center justify-start ">
+
+            </div>
+            {/* Middle  */}
+            {/* Right side */}
+        </div>
+    )
+}
